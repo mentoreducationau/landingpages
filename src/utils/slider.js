df0f38084aa67f5b32d06aeaf73b6e948c0e05b1
marek <marekreid@gmail.com>
2021-08-04 11:37:52 +0200

    updated static slider array

--- src/utils/slider.js
+++ src/utils/slider.js
@@ -1,20 +1,22 @@
 export const testimonialarray = [
     {
         imageurl: "/static1.jpeg" ,
-        name: "Melissaone",
+        name: "Melissa",
         jobtitle: "Project Management Leader",
         info: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour."
     },
     {
         imageurl: "/static2.jpeg" ,
-        name: "Melissatwo",
+        name: "Elizebeth",
         jobtitle: "Project Management Leader",
-        info: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour."
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eveniet, hic molestias quis quia ipsa fuga tempore provident beatae debitis doloribus esse soluta porro laborum sunt eos reiciendis aliquid nihil. Itaque officiis unde ut fugiat ipsum, modi quibusdam aut, quaerat dolorum labore iure temporibus ex quae quia ipsa ducimus similique"
     },
+    
     {
         imageurl: "/static3.jpeg" ,
-        name: "Melissathree",
-        jobtitle: "Project Management Leader",
-        info: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour."
+        name: "Emma",
+        jobtitle: "Investment Banker",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ea itaque, consequatur sit id nemo tenetur tempora culpa modi deserunt, eligendi repellat minima veniam fugit."
     }
 ]
+
