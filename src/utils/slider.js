ee014f11371006f3da674ac133a7361c85bfd8af
khalilcodes <khalilali.j@gmail.com>
2021-08-08 16:22:42 +0300

    modified image url paths

--- src/utils/slider.js
+++ src/utils/slider.js
@@ -1,19 +1,19 @@
 export const testimonialarray = [
     {
-        imageurl: "/static1.jpeg" ,
+        imageurl: "/melissa.jpg",
         name: "Melissa",
         jobtitle: "Project Management Leader",
         info: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour."
     },
     {
-        imageurl: "/static2.jpeg" ,
+        imageurl: "/elizabeth.jpg",
         name: "Elizebeth",
         jobtitle: "Project Management Leader",
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eveniet, hic molestias quis quia ipsa fuga tempore provident beatae debitis doloribus esse soluta porro laborum sunt eos reiciendis aliquid nihil. Itaque officiis unde ut fugiat ipsum, modi quibusdam aut, quaerat dolorum labore iure temporibus ex quae quia ipsa ducimus similique"
     },
     
     {
-        imageurl: "/static3.jpeg" ,
+        imageurl: "/emma.jpg",
         name: "Emma",
         jobtitle: "Investment Banker",
         info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ea itaque, consequatur sit id nemo tenetur tempora culpa modi deserunt, eligendi repellat minima veniam fugit."
