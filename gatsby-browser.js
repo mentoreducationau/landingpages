5b3ac58f563ac69b9c23bc05709ad83eae0ba890
khalilcodes <khalilali.j@gmail.com>
2021-08-09 22:43:43 +0300

    added typography

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,12 +10,14 @@ import { ThemeProvider } from 'styled-components'
 import Theme from './src/constant/theme'
 import GlobalStyles from './src/styles/GlobalStyles.css'
 import { useWindowSize } from './src/hooks/useWindowSize'
+import { Typography } from './src/styles/Typography.css'
 
 const App = ({ element }) => {
   const [width] = useWindowSize()
   return (
     <ThemeProvider theme={width < 1200 ? Theme.mobile : Theme.desktop}>
       <GlobalStyles />
+      <Typography />
       {element}
     </ThemeProvider>
   )
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,6 +11,7 @@ import { ThemeProvider } from 'styled-components'
 import Theme from './src/constant/theme'
 import GlobalStyles from './src/styles/GlobalStyles.css'
 import { useWindowSize } from './src/hooks/useWindowSize'
+import { Typography } from './src/styles/Typography.css'
 
 const App = ({ element }) => {
   const [width] = useWindowSize()
@@ -25,6 +26,7 @@ const App = ({ element }) => {
   return (
     <ThemeProvider theme={width < 1200 ? Theme.mobile : Theme.desktop}>
       <GlobalStyles />
+      <Typography />
       {element}
     </ThemeProvider>
   )
