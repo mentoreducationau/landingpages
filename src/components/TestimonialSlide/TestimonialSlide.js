302af3eadfe38a7d66028dd7fc6f56a88d5a9663
khalilcodes <khalilali.j@gmail.com>
2021-08-10 22:23:14 +0300

    added svg quotations

--- src/components/TestimonialSlide/TestimonialSlide.js
+++ src/components/TestimonialSlide/TestimonialSlide.js
@@ -5,9 +5,11 @@ import {
   TestimonialSlideImage,
   TestimonialSlideInfo,
   Image,
-  TestimonialSlideArrow,
-  TestimonialRightSlideArrow
+  SliderArrowLeft,
+  SliderArrowRight
 } from "./TestimonialSlide.css"
+
+import { QuoteSvg } from '../../images/svg/quotations'
 
 const TestimonialSlide = (props) => {
 
@@ -19,12 +21,14 @@ const TestimonialSlide = (props) => {
         </TestimonialSlideImage>
         <TestimonialSlideInfo>
           <h4 dark>{props.data.name}</h4>
-          <div>{props.data.jobtitle}</div>
+          <span>{props.data.jobtitle}</span>
           <p>
+            <QuoteSvg left />
             {props.data.info}
+            <QuoteSvg right />
           </p>
-          <TestimonialSlideArrow onClick={() => props.prevSlide()} />
-          <TestimonialRightSlideArrow  onClick={() => props.nextSlide()} />
+          <SliderArrowLeft onClick={() => props.prevSlide()} />
+          <SliderArrowRight  onClick={() => props.nextSlide()} />
         </TestimonialSlideInfo>
       </TestimonialSlideContainer>
     </TestimonialSlideWrapper>
