6fcbe78268ccda8245ba6b222bcc142587b39e4a
khalilcodes <khalilali.j@gmail.com>
2021-08-10 03:01:15 +0300

    added heading and intro to form

--- src/components/FooterForm/FooterForm.js
+++ src/components/FooterForm/FooterForm.js
@@ -8,12 +8,38 @@ import {
   InputLabel,
   Textarea,
   FooterFormbutton,
-  FooterFormbuttonWrapper
+  FooterFormbuttonWrapper,
+  FormImage,
+  FormIntroContainer,
+  FormDescription
 } from "./FooterForm.css"
+
+import { Title } from '../../styles/Typography.css'
 
 const FooterForm = () => {
   return (
     <FooterformContainer>
+      <Title weight={700} color="#707070">
+        Need more <span style={{ color: "#FF1010" }}>assistance</span> or to
+        talk to an expert?
+      </Title>
+      <FormIntroContainer>
+        <FormImage src="/footer-img.jpg" alt="form-image" />
+        <FormDescription>
+          <h4>We understand</h4>
+          <div>Help</div>
+          <p>
+            Sometimes you just want to talk to someone who understands what all
+            of this is information means and can explain it in a way you can
+            understand.
+          </p>
+          <div>Can’t find enough information?</div>
+          <p>
+            If you have more questions or queries we have Education Advise
+            experts waiting to answer any and all of your queries.
+          </p>
+        </FormDescription>
+      </FormIntroContainer>
       <InputContainer>
         <QuotedBoxLeft>
           <InputLabel>Name</InputLabel>
@@ -29,7 +55,6 @@ const FooterForm = () => {
         <Textarea />
         <FooterFormbuttonWrapper>
           <FooterFormbutton>REACH OUT</FooterFormbutton>
-
         </FooterFormbuttonWrapper>
       </InputContainer>
     </FooterformContainer>
