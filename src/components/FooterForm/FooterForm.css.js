3718d2140065371e34afd6f627ba26098b79d28d
khalilcodes <khalilali.j@gmail.com>
2021-08-10 03:02:07 +0300

    wip: heading and intro styles

--- src/components/FooterForm/FooterForm.css.js
+++ src/components/FooterForm/FooterForm.css.js
@@ -9,9 +9,58 @@ export const FooterformContainer = styled.div`
     text-align: center;
   }
 `
+
+export const FormIntroContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 43px;
+  padding: 0 81px;
+  margin-top: 41px;
+`
+
+export const FormDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h4 {
+    margin-bottom: 10px;
+    font-weight: 900;
+    font-size: 36px;
+    line-height: 46px;
+    letter-spacing: 0px;
+    text-align: left;
+    letter-spacing: 0px;
+    color: #707070;
+    opacity: 1;
+  }
+
+  p {
+    line-height: 23px;
+    color: #535353;
+    letter-spacing: 0px;
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  div {
+    letter-spacing: 0px;
+    color: #cc4a28;
+    opacity: 1;
+    font-size: 27px;
+    font-weight: 900;
+    margin: 6px 0;
+    line-height: 33px;
+  }
+`
+
+export const FormImage = styled.img`
+  width: 100%;
+`
+
 export const InputForm = styled.input`
   border: 3px solid #707070;
-  height: 79px;
+  height: 59px;
   margin-bottom: 23px;
   width: 100%;
   padding-left: 9px;
@@ -19,7 +68,8 @@ export const InputForm = styled.input`
 `
 
 export const InputLabel = styled.label`
-  font-size:28px;
+  font-size: 21px;
+  line-height: 26px;
   letter-spacing: 0px;
   color: #464343;
   margin-bottom: 15px;
@@ -51,7 +101,7 @@ export const QuotedBoxRight = styled.div`
 `
 
 export const Textarea = styled.textarea`
-  height: 265px;
+  height: 200px;
   border: 3px solid #707070;
   width: 100%;
   padding-left: 9px;
@@ -64,13 +114,14 @@ export const FooterFormbuttonWrapper = styled.div`
 `
 
 export const FooterFormbutton = styled.button`
-  background: #ff0000;
+  background: #cc4a28;
   border: none;
   border-radius: 2px;
   font-size: 40px;
-  font-family: Futura;
+  font-family: Futura Heavy;
   letter-spacing: 0px;
   color: #ffffff;
   text-transform: uppercase;
   padding: 20px 50px;
+  cursor: pointer;
 `
