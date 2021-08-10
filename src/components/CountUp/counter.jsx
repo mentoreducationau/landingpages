2fdc8d1f81a24bf6bf5da1bc7ba59acbfe87ccd0
khalilcodes <khalilali.j@gmail.com>
2021-08-11 01:31:26 +0300

    added scroll trigger to counter with option to set offset value

--- src/components/CountUp/counter.jsx
+++ src/components/CountUp/counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useLayoutEffect, useCallback } from 'react'
 import { CounterText } from './counter.css'
 
 const easeOutSine = t => -(Math.cos(Math.PI * t) - 1) / 2; // from easings.net
@@ -12,28 +12,47 @@ const frameDuration = 1000 / 60;
         weight  =>  @font-weight (default = 900)
 */
 
-const CountUp = ({ children, duration = 5000, ...props }) => {
+const CountUp = ({ children, duration = 5000, offset = 300, ...props }) => {
 
+    let textRef = useRef(null)
     const targetNumber = parseInt(children, 10)
+    const [hasScrolled, setScrolled] = useState(false)
     const [displayNumber, setDisplayNumber] = useState(0)
+
+    useLayoutEffect(() => {
+        const element = textRef.current
+        const elementPos = element.getBoundingClientRect().top
+
+        const onScroll = () => {
+            const scrollPos = window.scrollY + window.innerHeight - offset
+            if (elementPos < scrollPos) {
+                setScrolled(true)
+            }
+        }
+
+        window.addEventListener('scroll', onScroll)
+        return () => window.removeEventListener('scroll', onScroll)
+    },[])
 
     useEffect(() => {
         let frame = 0;
-		const totalFrames = Math.round( duration / frameDuration )
+        const totalFrames = Math.round( duration / frameDuration )
 
-        const counter = setInterval(() => {
-            frame++
-            var progress = easeOutSine(frame / totalFrames)
-            setDisplayNumber(targetNumber * progress)
-
-            if (frame === totalFrames) {
-                clearInterval(counter)
-            }
-        },frameDuration)
-    },[duration, targetNumber])
+        if (hasScrolled) {
+            const counter = setInterval(() => {
+                frame++
+                var progress = easeOutSine(frame / totalFrames)
+                setDisplayNumber(targetNumber * progress)
+    
+                if (frame === totalFrames) {
+                    clearInterval(counter)
+                }
+            },frameDuration)
+        }
+    },[hasScrolled, duration, targetNumber])
     
     return (
-        <CounterText {...props}>
+        <CounterText ref={textRef} {...props}>
             {Math.floor(displayNumber)}
         </CounterText>
     )
