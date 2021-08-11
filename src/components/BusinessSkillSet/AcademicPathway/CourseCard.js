a4d6e15d071e7775869fe4cf6a8bd2d47cc0bcf4
khalilcodes <khalilali.j@gmail.com>
2021-08-12 02:49:10 +0300

    added logic to display single and dual courses

--- src/components/BusinessSkillSet/AcademicPathway/CourseCard.js
+++ src/components/BusinessSkillSet/AcademicPathway/CourseCard.js
@@ -1,28 +1,56 @@
-import React from 'react'
+import React from "react"
 
-import CourseCardModal from './Courses/CourseCardModal'
-import { CourseCardWrapper, CourseCardTitle, CourseCardImg, CourseCardButton, CourseCardButtonContainer } from './AcademicPathway.css'
+import CourseCardModal from "./Courses/CourseCardModal"
+import {
+  CourseCardWrapper,
+  CourseCardTitle,
+  CourseCardImg,
+  CourseCardButton,
+  CourseCardButtonContainer,
+} from "./AcademicPathway.css"
 
-import cardEg from '../../../images/card-imgs/acc/eg.png'
+import cardEg from "../../../images/card-imgs/acc/eg.png"
 
-const CourseCard = ({course}) => {
-console.log(`course`, course)
-const [displayModal, setDisplayModal] = React.useState(false)
-    return (
-        <div style={{overflow: `hidden`}}>
-       <CourseCardWrapper>
-           <CourseCardImg src={cardEg}/>
-           <CourseCardTitle>{course.courseCode} - {course.courseName}</CourseCardTitle>
-           <CourseCardButtonContainer>
-           <CourseCardButton>Course Guide</CourseCardButton>
-           <CourseCardButton
-           onClick={() => setDisplayModal(true)}
-           >Find Out More</CourseCardButton>
-           </CourseCardButtonContainer>
-       </CourseCardWrapper>
-       <CourseCardModal display={displayModal} course={course} setDisplayModal={setDisplayModal}/>
-       </div>
-    )
+const CourseCard = ({ course, dualCourse = false }) => {
+//   console.log(`course`, course)
+  const [displayModal, setDisplayModal] = React.useState(false)
+
+  if (course === []) return null
+
+  return (
+    <div style={{ width: `100%`, height: `100%` }}>
+      <CourseCardWrapper>
+        <CourseCardImg src={cardEg} />
+        {!dualCourse && (
+          <CourseCardTitle>
+            <h4>{course.courseCode} —</h4> {course.courseName}
+          </CourseCardTitle>
+        )}
+        {dualCourse && (
+          <>
+            <CourseCardTitle>{course.courseName}</CourseCardTitle>
+            <CourseCardTitle>
+              {course.courseA.courseCode} - {course.courseA.courseName}
+            </CourseCardTitle>
+            <CourseCardTitle>
+              {course.courseB.courseCode} - {course.courseB.courseName}
+            </CourseCardTitle>
+          </>
+        )}
+        <CourseCardButtonContainer>
+          {/* <CourseCardButton>Course Guide</CourseCardButton> */}
+          <CourseCardButton onClick={() => setDisplayModal(true)}>
+            Find Out More
+          </CourseCardButton>
+        </CourseCardButtonContainer>
+      </CourseCardWrapper>
+      <CourseCardModal
+        display={displayModal}
+        course={course}
+        setDisplayModal={setDisplayModal}
+      />
+    </div>
+  )
 }
 
 export default CourseCard
