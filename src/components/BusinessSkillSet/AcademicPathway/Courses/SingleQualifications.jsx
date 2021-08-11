745fe0059b07709cec90d21572eee494aa0c3b22
khalilcodes <khalilali.j@gmail.com>
2021-08-12 02:54:52 +0300

    renamed files

---
+++ src/components/BusinessSkillSet/AcademicPathway/Courses/SingleQualifications.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react"
+import { CardsWrapper } from "../AcademicPathway.css"
+import CourseCard from "../CourseCard"
+
+const SingleQualifications = ({ courses }) => {
+  const [singleCourses, setSingleCourses] = React.useState([])
+  // if (courses[6].type == "dualQualification") console.log("meh")
+
+  useEffect(() => {
+    const newSingleCourses = []
+    courses &&
+      courses.map(course => {
+        if (course.type !== "dualQualification") newSingleCourses.unshift(course)
+      })
+    setSingleCourses(newSingleCourses)
+  }, [courses])
+
+  return (
+    <CardsWrapper>
+      {singleCourses.map((course, i) => (
+        <CourseCard course={course} key={i} />
+      ))}
+    </CardsWrapper>
+  )
+}
+
+export default SingleQualifications
