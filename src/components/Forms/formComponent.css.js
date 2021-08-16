bda4aef63dfeb6d504835df93d10b660d402595a
khalilcodes <khalilali.j@gmail.com>
2021-08-16 16:08:12 +0300

    add styles for new form component

---
+++ src/components/Forms/formComponent.css.js
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+import { GlobalFormField } from "../../styles/FormStyles.css"
+
+export const CustomFormStyled = styled.form`
+  width: 100%;
+  margin-bottom: 4rem;
+`
+
+export const FormField = styled(GlobalFormField)`
+  width: 100%;
+
+  label {
+    font: 28px/35px Futura Medium;
+    color: #464343;
+    padding-left: 1rem;
+  }
+
+  input {
+    border: 3px solid #707070;
+  }
+`
+
+export const FormButton = styled.button`
+  font: 40px/51px Futura Heavy;
+  background: #cc4a28;
+  color: white;
+  border: 0;
+  border-radius: 2px;
+  width: 100%;
+  text-transform: uppercase;
+  margin: 3% auto 0;
+`
