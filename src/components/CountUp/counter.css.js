2ffa4aa872f70469ea8c7764b4c614f2a053ebac
khalilcodes <khalilali.j@gmail.com>
2021-08-21 15:23:47 +0300

    changed element to p tag from typography

--- src/components/CountUp/counter.css.js
+++ src/components/CountUp/counter.css.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components'
+import { ParagraphHeavy } from '../../styles/Typography.css'
 
-export const CounterText = styled.h2`
+export const CounterText = styled(ParagraphHeavy)`
   padding: 0;
   margin: 0;
   width: fit-content;
   height: fit-content;
-  color: ${props => (props.color ? props.color : "#ff0808")};
-  font-size: ${props => (props.size ? props.size : "18vw")};
-  font-family: ${props => (props.font ? props.font : "Futura")};
-  font-weight: ${props => (props.weight ? props.weight : "900")};
-  font: normal normal;
+  color: ${props => (props.color ? props.color : "")};
+  font-size: ${props => (props.size ? props.size : "")};
+  font-family: ${props => (props.font ? props.font : "")};
+  font-weight: ${props => (props.weight ? props.weight : "")};
+  /* font: normal normal; */
   letter-spacing: 0px;
 `
