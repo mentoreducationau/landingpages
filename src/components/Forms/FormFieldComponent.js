52899438a860c3e560e42bfdcec4c0938a73e9d1
khalilcodes <khalilali.j@gmail.com>
2021-08-31 15:42:14 +0300

    modified Form field inputs with onchange, values and defaults, added useForm for handling submit

--- src/components/Forms/FormFieldComponent.js
+++ src/components/Forms/FormFieldComponent.js
@@ -1,40 +1,83 @@
-import React, { useState } from 'react'
-import { CustomFormStyled, FormField, FormButton } from './formComponent.css'
-import { uniqueId } from 'lodash'
+import React, { useState } from "react"
+import PropTypes from 'prop-types'
+import { uniqueId } from "lodash"
+import useForm from "../../hooks/useForm"
+import { CustomFormStyled, FormField, FormButton } from "./formComponent.css"
 
 export const FormFieldComponent = ({
   children,
   label,
   name,
   type,
-  required = false,
+  onChange,
+  value,
+  error,
+  required,
 }) => {
   const [id] = useState(() => uniqueId(`${name}-`))
   return (
-      <>
-        <label htmlFor={id}>
-            {label} {children}
-        </label>
-        <input type={type} name={name} id={id} aria-required={required} />
-      </>
+    <>
+      <label htmlFor={id}>
+        {label} {children}
+      </label>
+      <input
+        id={id}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        aria-required={required}
+        style={error && { border: "solid 1px red" }}
+      />
+      {error && <p style={{ color: "red", fontSize: "8px" }}>{error}</p>}
+    </>
   )
 }
 
-export const CustomForm = ({ buttonText, value, name, onClick }) => {
+FormFieldComponent.defaultProps = {
+  type: "text",
+  required: false,
+}
+
+FormFieldComponent.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(["text", "email", "number", "password"]),
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+}
+
+export const CustomForm = ({ buttonText, handleSubmit }) => {
+  const { values, onSubmit, handleChange } = useForm(() => handleSubmit(values))
   return (
-    <CustomFormStyled action="">
+    <CustomFormStyled onSubmit={onSubmit}>
       <FormField>
-        <FormFieldComponent label="Name" name="name" type="text" />
+        <FormFieldComponent
+          label="Name"
+          name="name"
+          value={values.name || ""}
+          onChange={handleChange}
+        />
       </FormField>
       <FormField>
-        <FormFieldComponent label="Email" name="email" type="email" />
+        <FormFieldComponent
+          label="Email"
+          name="email"
+          type="email"
+          value={values.email || ""}
+          onChange={handleChange}
+        />
       </FormField>
       <FormField>
-        <FormFieldComponent label="Phone" name="phone" type="text" />
+        <FormFieldComponent
+          label="Phone"
+          name="phone"
+          value={values.phone || ""}
+          onChange={handleChange}
+        />
       </FormField>
-      <FormButton value={value} type="submit" name={name} onClick={onClick}>
-        {buttonText}
-      </FormButton>
+      <FormButton>{buttonText}</FormButton>
     </CustomFormStyled>
   )
 }
